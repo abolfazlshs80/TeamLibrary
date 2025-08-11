64b376321dc439b44d86716b61496030c93547f2
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-08-11 18:50:43 +0330

    Add User Models

---
+++ Team-Library/BackEnd/TeamLibrary.API/Data/Models/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace TeamLibrary.API.Data.Models;
+
+public class Users
+{
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+    public required string Password { get; set; }
+}
