46978ea5b12ad8ce21b3baddca7baa625259d76f
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-08-11 18:51:17 +0330

    Add dbcontext And EfCore

---
+++ Team-Library/BackEnd/TeamLibrary.API/Data/Context/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+using TeamLibrary.API.Data.Models;
+
+namespace TeamLibrary.API.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+    }
+
+    #region DbSet
+
+    public DbSet<Users> Users { get; set; }
+
+
+
+    #endregion
+
+
+}
