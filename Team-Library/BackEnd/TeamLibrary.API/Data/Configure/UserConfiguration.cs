d04f328618fe55aa6c97065ce76af499229c9743
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-08-11 18:50:55 +0330

    Add UserConfigure

---
+++ Team-Library/BackEnd/TeamLibrary.API/Data/Configure/UserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TeamLibrary.API.Data.Models;
+
+namespace Diet.Persistence.EF.Configurations.User;
+
+public class UserConfiguration : IEntityTypeConfiguration<Users>
+{
+    public void Configure(EntityTypeBuilder<Users> builder)
+    {
+
+
+    }
+
+}
