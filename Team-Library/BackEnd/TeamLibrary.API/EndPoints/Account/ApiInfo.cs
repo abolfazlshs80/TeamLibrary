e5e6046cc00d8f5660ccaa4dc34267b44268c211
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-08-14 20:32:31 +0330

    ✨ feat(account): implement create account endpoint
    - add new endpoint for user registration
    - implement request and response contracts
    - integrate with asp.net identity for user management
    - add role assignment for new users
    - return user information upon successful creation

---
+++ Team-Library/BackEnd/TeamLibrary.API/EndPoints/Account/ApiInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace TeamLibrary.API.EndPoints.Account
+{
+    public static class ApiInfo
+    {
+        public const string Tag = "Account";
+        public const string Prefix = "/api/Account";
+    }
+}
