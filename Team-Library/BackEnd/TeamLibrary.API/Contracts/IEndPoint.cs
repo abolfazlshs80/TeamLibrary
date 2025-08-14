926a66e10c589f4e92d2964fa53df2522c9cb00d
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-08-14 20:32:03 +0330

    ✨ feat(api): create base endpoint and interface
    - create base endpoint to handle standard api responses
    - create interface to implement endpoints

---
+++ Team-Library/BackEnd/TeamLibrary.API/Contracts/IEndPoint.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Net;
+using TeamLibrary.API.Tools.Api;
+
+namespace TeamLibrary.API.Contracts;
+
+
+public interface IEndpoint 
+{
+    void MapEndpoint(IEndpointRouteBuilder app);
+}
+public abstract class BaseEndpoint
+{
+    protected Ok<ApiResponse> Ok(string message)
+      => TypedResults.Ok(new ApiResponse(message));
+
+    protected IResult Ok(object? data)
+        => TypedResults.Ok(new ApiResponse(data));
+
+    protected IResult Ok(object data, string message)
+        => TypedResults.Ok(new ApiResponse(data, message));
+
+    protected IResult BadRequest(string message)
+        => TypedResults.BadRequest(new ApiResponse(message, HttpStatusCode.BadRequest));
+
+    protected IResult BadRequest(string[] messages)
+        => TypedResults.BadRequest(new ApiResponse(messages));
+
+    protected IResult BadRequest(ValidationResult result)
+        => BadRequest(result.Errors.Select(e => e.ErrorMessage).ToArray());
+
+    protected IResult NotFound(string message)
+        => TypedResults.NotFound(new ApiResponse(message, HttpStatusCode.NotFound));
+
+    protected IResult Unauthorized(string message = "Unauthorized")
+        => TypedResults.Unauthorized();
+}
