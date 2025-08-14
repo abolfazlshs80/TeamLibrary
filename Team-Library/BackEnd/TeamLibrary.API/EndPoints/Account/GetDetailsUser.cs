e8f03981d3c64f3f1ed670fcb755b9dac1588cde
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-08-14 20:32:37 +0330

    ✨ feat(account): implement get user details endpoint
    - create endpoint to retrieve user details
    - add route mapping and basic handler

---
+++ Team-Library/BackEnd/TeamLibrary.API/EndPoints/Account/GetDetailsUser.cs
@@ -0,0 +1,24 @@
+﻿using TeamLibrary.API.Contracts;
+
+namespace TeamLibrary.API.EndPoints.Account;
+
+public static class GetDetailsUser
+{
+    public class EndPoint : BaseEndpoint, IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet($"{ApiInfo.Prefix}/GetDetailsUser", handler: async (
+                
+                     HttpContext context
+                ) =>
+            {
+             
+                    return Ok(12);
+
+            })
+                //.RequireAuthorization()
+                .WithTags(ApiInfo.Tag);
+        }
+    }
+}
