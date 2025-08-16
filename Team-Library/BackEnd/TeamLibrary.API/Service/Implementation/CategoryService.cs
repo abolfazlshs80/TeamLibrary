9006328992f5ee28b66ce5c530c27dee4fc1342e
m.faraji9841@gmail.com <m.faraji9841@gmail.com>
2025-08-16 12:15:27 +0330

    writing Method GetAllCategoryAsync in ICategoryService

---
+++ Team-Library/BackEnd/TeamLibrary.API/Service/Implementation/CategoryService.cs
@@ -0,0 +1,15 @@
+﻿using TeamLibrary.API.Data.Models;
+using TeamLibrary.API.Data.Repository.Interface;
+using TeamLibrary.API.Service.Interface;
+
+namespace TeamLibrary.API.Service.Implementation
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _repository;
+        public Task<List<Category>> GetAllCategoryAsync()
+        {
+            return _repository.GetAllCategoryAsync();
+        }
+    }
+}
---
+++ Team-Library/BackEnd/TeamLibrary.API/Service/Interface/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿using TeamLibrary.API.Data.Models;
+
+namespace TeamLibrary.API.Service.Interface
+{
+    public interface ICategoryService
+    {
+        Task<List<Category>> GetAllCategoryAsync();
+    }
+}
