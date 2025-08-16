2d536bc390fe81a51b2dca1f39f87126240674b2
m.faraji9841@gmail.com <m.faraji9841@gmail.com>
2025-08-16 12:25:05 +0330

    writing Method DeleteCategoryAsync in ICategoryRepository

--- Team-Library/BackEnd/TeamLibrary.API/Service/Implementation/CategoryService.cs
+++ Team-Library/BackEnd/TeamLibrary.API/Service/Implementation/CategoryService.cs
@@ -7,9 +7,36 @@ namespace TeamLibrary.API.Service.Implementation
     public class CategoryService : ICategoryService
     {
         private readonly ICategoryRepository _repository;
+
+        public async Task AddCategoryAsync(Category category)
+        {
+            await _repository.AddCategoryAsync(category);
+        }
+
+        public async Task DeleteCategoryAsync(Category category)
+        {
+            await _repository.DeleteCategoryAsync(category);
+
+        }
+
+        public Task<bool> DeleteCategoryByIdAsync(int categoryId)
+        {
+            
+        }
+
         public Task<List<Category>> GetAllCategoryAsync()
         {
             return _repository.GetAllCategoryAsync();
+        }
+
+        public Task<Category> GetCategoryByIdAsync(int categoryId)
+        {
+            return _repository.GetCategoryByIdAsync(categoryId);
+        }
+
+        public Task UpdateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Team-Library/BackEnd/TeamLibrary.API/Service/Interface/ICategoryService.cs
+++ Team-Library/BackEnd/TeamLibrary.API/Service/Interface/ICategoryService.cs
@@ -5,5 +5,11 @@ namespace TeamLibrary.API.Service.Interface
     public interface ICategoryService
     {
         Task<List<Category>> GetAllCategoryAsync();
+        Task<Category> GetCategoryByIdAsync(int categoryId);
+        Task AddCategoryAsync(Category category);
+        Task UpdateCategoryAsync(Category category);
+        Task DeleteCategoryAsync(Category category);
+        Task<bool> DeleteCategoryByIdAsync(int categoryId);
+
     }
 }
