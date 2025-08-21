c31534f99eae7f5b0a5e1a6b42cc75f6f2a1e42a
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-08-21 19:54:02 +0330

    ✨ feat(category): add DeleteCategoryRequestDto
    - create DeleteCategoryRequestDto for delete category feature
    - add required attribute for Id property

---
+++ Team-Library/BackEnd/TeamLibrary.API/Featrues/Category/DTOs/Request/DeleteCategoryRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeamLibrary.API.Featrues.Category.DTOs;
+
+public class DeleteCategoryRequestDto
+{
+    [Required(ErrorMessage = "شناسه دسته‌بندی الزامی است")]
+    public int Id { get; set; }
+
+}
