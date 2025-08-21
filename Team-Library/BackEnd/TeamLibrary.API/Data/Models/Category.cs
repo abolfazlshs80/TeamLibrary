72e78ffc3f36ff5b8b5372f1115795f9184f4930
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-08-21 19:53:13 +0330

    ♻️ refactor(category): inherit from base entity
    - refactor category model to inherit from BaseEntity to inherit common properties like Id

--- Team-Library/BackEnd/TeamLibrary.API/Data/Models/Category.cs
+++ Team-Library/BackEnd/TeamLibrary.API/Data/Models/Category.cs
@@ -1,8 +1,8 @@
 ﻿namespace TeamLibrary.API.Data.Models
 {
-    public class Category
+    public class Category:BaseEntity
     {
-        public int Id { get; set; }
+
 
         public string Name { get; set; } = string.Empty;
         public string Slug { get; set; } = string.Empty;
