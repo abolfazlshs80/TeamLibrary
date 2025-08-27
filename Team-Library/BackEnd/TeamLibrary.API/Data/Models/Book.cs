861ffac219bd46b4392de1986c82119f67a4f401
m.faraji9841@gmail.com <m.faraji9841@gmail.com>
2025-08-27 09:52:52 +0330

    Book model updated

--- Team-Library/BackEnd/TeamLibrary.API/Data/Models/Book.cs
+++ Team-Library/BackEnd/TeamLibrary.API/Data/Models/Book.cs
@@ -1,22 +1,20 @@
 ﻿namespace TeamLibrary.API.Data.Models;
 
-public class Book:BaseEntity
+public class Book : BaseEntity
 {
-
-
     public string Title { get; set; } = null!;
     public string Slug { get; set; } = null!;
-
     public int CategoryId { get; set; }
     public string ImagePath { get; set; } = null!;
-
-
     public string PdfPath { get; set; } = null!;
-
-
     public string Description { get; set; } = null!;
-
     public int? UserId { get; set; }
+    public int Rank { get; set; } 
+    public int PublicationYear { get; set; } 
+    public string Author { get; set; } 
+    public int Pages { get; set; } 
+    public decimal Price { get; set; } 
+    public string Language { get; set; } 
 
     public Category Category { get; set; } = null!;
     public Users? User { get; set; }
