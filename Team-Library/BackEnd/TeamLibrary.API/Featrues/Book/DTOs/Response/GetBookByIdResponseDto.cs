d3f2aa43d75955e25467cf48875cd46306959e99
Mohammad Faraji <m.faraji9841@gmail.com>
2025-08-28 00:32:17 +0330

    Create GetBookByIdResponseDto

---
+++ Team-Library/BackEnd/TeamLibrary.API/Featrues/Book/DTOs/Response/GetBookByIdResponseDto.cs
@@ -0,0 +1,18 @@
+﻿namespace TeamLibrary.API.Featrues.Book.DTOs.Response
+{
+    public class GetBookByIdResponseDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+        public int Pages { get; set; }
+        public decimal Price { get; set; }
+        public string Language { get; set; }
+        public string ImagePath { get; set; }
+        public string PdfPath { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
