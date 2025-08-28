6d6b6f38979526379153166546824538ee044af7
Mohammad Faraji <m.faraji9841@gmail.com>
2025-08-28 22:42:12 +0330

    Create GetBookListRequestDto

--- Team-Library/BackEnd/TeamLibrary.API/Featrues/Book/DTOs/Response/DeleteBookResponseDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TeamLibrary.API.Featrues.Book.DTOs.Response
-{
-    public class DeleteBookResponseDto
-    {
-        public bool IsSuccess { get; set; }
-        public string Message { get; set; }
-    }
-}
