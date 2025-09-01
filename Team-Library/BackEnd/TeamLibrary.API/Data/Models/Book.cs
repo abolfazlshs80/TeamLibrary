4b6cf9b44cb7155202b8fce5dd18ac3718cfa277
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-09-01 21:09:23 +0330

    ✨ feat(book): add default values for book properties
    - set default values to empty strings for string properties in the Book model
    - ensures properties are initialized, preventing null reference exceptions

--- Team-Library/BackEnd/TeamLibrary.API/Data/Models/Book.cs
+++ Team-Library/BackEnd/TeamLibrary.API/Data/Models/Book.cs
@@ -2,20 +2,21 @@
 
 public class Book : BaseEntity
 {
-    public string Title { get; set; }
-    public string Slug { get; set; } 
+    public string Title { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
     public int CategoryId { get; set; }
-    public string ImagePath { get; set; } 
-    public string PdfPath { get; set; } 
-    public string Description { get; set; } 
+    public string ImagePath { get; set; } = string.Empty;
+    public string PdfPath { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public int? UserId { get; set; }
     public int Rank { get; set; } 
     public int PublicationYear { get; set; } 
-    public string Author { get; set; } 
+    public string Author { get; set; } =string.Empty;
     public int Pages { get; set; } 
-    public decimal Price { get; set; } 
-    public string Language { get; set; } 
+    public decimal Price { get; set; }
+    public string Language { get; set; } = string.Empty;
+    public string Translator { get; set; } = string.Empty;
 
-    public Category Category { get; set; } 
+    public Category Category { get; set; } = new();
     public Users? User { get; set; }
 }
