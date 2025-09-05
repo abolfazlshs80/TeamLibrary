aaae6ca1274402de3048816dc40ee15621b5766c
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-09-05 09:50:37 +0330

    use swagger on production mode

--- Team-Library/BackEnd/TeamLibrary.API/Program.cs
+++ Team-Library/BackEnd/TeamLibrary.API/Program.cs
@@ -24,10 +24,10 @@ app.UseMiddleware<ExceptionHandlingMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
+}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
