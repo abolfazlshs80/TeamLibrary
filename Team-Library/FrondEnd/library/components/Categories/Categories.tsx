40ba6ba99debe3e66298af087829fdd489981642
farnoosh-krm <farnoosh.karimi99@gmail.com>
2025-09-06 02:22:38 +0330

    (feat) - GetAllCategories API added to main page

--- Team-Library/FrondEnd/library/components/Categories/Categories.tsx
+++ Team-Library/FrondEnd/library/components/Categories/Categories.tsx
@@ -1,10 +1,38 @@
 "use client";
-import { BookCategories } from "@/modal/BookCategories";
 import { libraryRoutes } from "@/routes";
+import axios from "axios";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BeatLoader } from "react-spinners";
+
+interface Category {
+  id: number;
+  name: string;
+  slug: string;
+}
 
 const Categories = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/Category/GetAllCategory?PageNumber=1&PageSize=100`
+        );
+        setCategories(res.data.data.list);
+        console.log("Fetched categories:", res.data);
+      } catch (error) {
+        console.error("Fetching categories failed:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCourses();
+  }, []);
+
   const router = useRouter();
   return (
     <div className="mt-16 max-w-7xl mx-auto">
@@ -12,20 +40,26 @@ const Categories = () => {
         دسته بندی کتاب ها بر اساس موضوع
       </h4>
 
-      <div className="flex flex-wrap gap-3">
-        {BookCategories.map((category) => (
-          <div
-            key={category.id}
-            onClick={() => {
-              router.push(libraryRoutes.explore);
-              localStorage.setItem("selectedCategory", category.value);
-            }}
-            className="border-2 cursor-pointer border-[#435F56] px-6 py-2 text-sm font-semibold text-[#435F56] rounded-full hover:bg-[#435F56] hover:text-white transition-colors duration-300 text-center"
-          >
-            {category.category}
-          </div>
-        ))}
-      </div>
+      {loading ? (
+        <div className="flex items-center justify-center">
+          <BeatLoader color="#d4b091" />
+        </div>
+      ) : (
+        <div className="flex flex-wrap gap-3">
+          {categories.map((category) => (
+            <div
+              key={category.id}
+              onClick={() => {
+                router.push(libraryRoutes.explore);
+                localStorage.setItem("selectedCategory", category.name);
+              }}
+              className="border-2 cursor-pointer border-[#435F56] px-6 py-2 text-sm font-semibold text-[#435F56] rounded-full hover:bg-[#435F56] hover:text-white transition-colors duration-300 text-center"
+            >
+              {category.name}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
