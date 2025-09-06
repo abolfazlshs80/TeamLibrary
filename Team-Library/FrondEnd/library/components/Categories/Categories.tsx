b6dadf7dc13ed33d251413bc5493cc9e3926b1b6
farnoosh-krm <farnoosh.karimi99@gmail.com>
2025-09-06 03:34:26 +0330

    (feat) - GetAllBooks API added to explore ExplorePage and find books with category (err) - image problem

--- Team-Library/FrondEnd/library/components/Categories/Categories.tsx
+++ Team-Library/FrondEnd/library/components/Categories/Categories.tsx
@@ -52,6 +52,10 @@ const Categories = () => {
               onClick={() => {
                 router.push(libraryRoutes.explore);
                 localStorage.setItem("selectedCategory", category.name);
+                localStorage.setItem(
+                  "selectedCategoryId",
+                  (category.id).toString()
+                );
               }}
               className="border-2 cursor-pointer border-[#435F56] px-6 py-2 text-sm font-semibold text-[#435F56] rounded-full hover:bg-[#435F56] hover:text-white transition-colors duration-300 text-center"
             >
--- Team-Library/FrondEnd/library/next.config.ts
+++ Team-Library/FrondEnd/library/next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "abolfazl11111.runasp.net",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    // domains: ["abolfazl11111.runasp.net"],
+    unoptimized: true,
+  },
   /* config options here */
 };
 
