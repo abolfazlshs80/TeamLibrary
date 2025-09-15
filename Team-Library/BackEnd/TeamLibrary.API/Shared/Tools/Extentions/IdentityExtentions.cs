bb11fed2b12cc4fb185cc4f875f2a970e313a8c7
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-09-15 17:23:47 +0330

    ✨ feat(auth): implement identity extension for user ID retrieval
    - add IdentityExtentions class to provide extension methods for ClaimsPrincipal
    - add GetId method to retrieve user ID based on UserType from claims

---
+++ Team-Library/BackEnd/TeamLibrary.API/Shared/Tools/Extentions/IdentityExtentions.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+using TeamLibrary.API.Data.Models;
+
+namespace TeamLibrary.API.Shared.Tools.Extentions;
+
+public static class IdentityExtentions
+{
+    public static string GetId(this ClaimsPrincipal? user, UserType type)
+
+    {
+        if (user == null)
+            return null;
+        return user.FindFirst(type.ToString() + "Id")?.Value;
+    }
+}
