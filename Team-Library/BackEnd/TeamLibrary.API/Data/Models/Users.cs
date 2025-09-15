1eba29deee121a1cf7bf46d554a7f63829ebf4f3
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-09-15 18:23:59 +0330

    Add FullName && Email

--- Team-Library/BackEnd/TeamLibrary.API/Data/Models/Users.cs
+++ Team-Library/BackEnd/TeamLibrary.API/Data/Models/Users.cs
@@ -4,6 +4,8 @@ public class Users:BaseEntity
 {
 
     public required string UserName { get; set; }
+    public string? FullName { get; set; } = string.Empty;
+    public string? Email { get; set; } = string.Empty;
     public required string Password { get; set; }
     public List<Book> Books { get; set; } = new();
 }
