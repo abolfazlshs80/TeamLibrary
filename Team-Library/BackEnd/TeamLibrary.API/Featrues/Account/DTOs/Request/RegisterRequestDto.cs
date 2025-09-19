b57a9dfb0dc38ed2d82ea4b102bd9c91b914d642
Mohammad Faraji <m.faraji9841@gmail.com>
2025-09-19 14:20:21 +0330

    Update RegisterRequestDto

--- Team-Library/BackEnd/TeamLibrary.API/Featrues/Account/DTOs/Request/RegisterRequestDto.cs
+++ Team-Library/BackEnd/TeamLibrary.API/Featrues/Account/DTOs/Request/RegisterRequestDto.cs
@@ -2,5 +2,9 @@
 {
     public class RegisterRequestDto
     {
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
     }
 }
