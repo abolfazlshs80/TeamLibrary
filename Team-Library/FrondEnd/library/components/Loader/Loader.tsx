177ba0d91803b867555e26d7e622f745f7e18cc9
HamidrezaHe <hamidrezahedayati1998@gmail.com>
2025-09-19 15:11:50 +0330

    refactor: update Loader component design and code

--- Team-Library/FrondEnd/library/components/Loader/Loader.tsx
+++ Team-Library/FrondEnd/library/components/Loader/Loader.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Loader = () => {
+export default function Loader() {
   return (
-    <div className="flex items-center justify-center">
-        <span className=" h-12 w-12 border-4 border-[#F0E1DE] border-t-transparent rounded-full animate-spin"></span>
+    <div className="flex items-center justify-center h-screen">
+      <span
+        className="h-12 w-12 border-4   rounded-full  "
+        style={{
+          display: "inline-block",
+          animation: "dynamicSpin 1s ease-in-out infinite",
+        }}
+      />
+      <style jsx>{`
+        @keyframes dynamicSpin {
+          0% { transform: rotate(36deg);border-color:#F0E1DE; border-top-color:transparent; border-right-color:transparent; border-bottom-color:transparent;}
+          25% { transform: rotate(150deg); border-color:#F0E1DE; border-top-color:transparent; border-right-color:transparent; }
+          50% { transform: rotate(324deg); border-color:#F0E1DE;border-top-color:transparent;}
+          100% { transform: rotate(360deg); border-color:#F0E1DE; border-top-color:transparent; border-right-color:transparent; border-bottom-color:transparent;}
+        }
+      `}</style>
     </div>
-  )
+  );
 }
-
-export default Loader
