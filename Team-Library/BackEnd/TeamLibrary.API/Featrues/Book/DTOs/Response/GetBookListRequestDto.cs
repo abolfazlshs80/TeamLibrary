2dacbd65696abf7cdb0f9a800cebaa217a5d633c
Mohammad Faraji <m.faraji9841@gmail.com>
2025-09-29 21:03:12 +0330

    Update GetBookListRequestDto

--- Team-Library/BackEnd/TeamLibrary.API/Featrues/Book/DTOs/Response/GetBookListRequestDto.cs
+++ Team-Library/BackEnd/TeamLibrary.API/Featrues/Book/DTOs/Response/GetBookListRequestDto.cs
@@ -2,7 +2,15 @@
 {
     public class GetBookListRequestDto
     {
-        public int PageNumber { get; set; } 
-        public int PageSize { get; set; }  
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public int? Rank { get; set; }
+        public int? MinPageCount { get; set; }
+        public int? MaxPageCount { get; set; }
+        public string? Language { get; set; }
+        public string? Author { get; set; }
+        public string? Category { get; set; }
+        public string? Search { get; set; }
     }
 }
