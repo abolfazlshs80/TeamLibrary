f236538e0f8ae1df2f9144159100e37e4f22aa73
abolfazl Shabani <57566177+abolfazlshs80@users.noreply.github.com>
2025-09-30 18:28:00 +0330

    ✨ feat(account): add reset and forget password dtos
    - add 【ResetPasswordRequestDto】 to handle password reset requests
    - add 【ForgetPasswordRequestDto】 to handle forget password requests

--- Team-Library/BackEnd/TeamLibrary.API/Features/Account/DTOs/Request/RegisterRequestDto.cs
+++ Team-Library/BackEnd/TeamLibrary.API/Features/Account/DTOs/Request/RegisterRequestDto.cs
@@ -1,10 +1,21 @@
-﻿namespace TeamLibrary.API.Featrues.Account.DTOs.Request
+﻿namespace TeamLibrary.API.Featrues.Account.DTOs.Request;
+
+public class RegisterRequestDto
 {
-    public class RegisterRequestDto
-    {
-        public string UserName { get; set; }
-        public string FullName { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-    }
+    public string UserName { get; set; }
+    public string FullName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+}
+public class ResetPasswordRequestDto
+{
+    public required string Email { get; set; }
+    public required string Code { get; set; }
+    public required string Password { get; set; }
+
+}
+public class ForgetPasswordRequestDto
+{
+
+    public  required string Email { get; set; }
 }
