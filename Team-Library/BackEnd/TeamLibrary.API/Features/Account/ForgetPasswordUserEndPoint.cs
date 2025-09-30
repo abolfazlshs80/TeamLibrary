1e67d9ed5535ff90216e0b68fafa800a45e4f887
abolfazl Shabani <57566177+abolfazlshs80@users.noreply.github.com>
2025-09-30 18:27:52 +0330

    ✨ feat(account): implement forget password endpoint
    - add endpoint for users to request a password reset
    - integrate validation filter for request DTO

---
+++ Team-Library/BackEnd/TeamLibrary.API/Features/Account/ForgetPasswordUserEndPoint.cs
@@ -0,0 +1,30 @@
+﻿using DrMeet.Api.Shared.Services.JwtService;
+using TeamLibrary.API.Featrues.Account.DTOs.Request;
+using TeamLibrary.API.Shared.Contracts;
+using TeamLibrary.API.Shared.Service.Interface;
+using TeamLibrary.API.Shared.Tools.Helper;
+
+namespace TeamLibrary.API.Featrues.Account;
+
+public static class ForgetPasswordUserEndPoint
+{
+    public class EndPoint : BaseEndpoint, IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost($"{ApiInfo.Prefix}/ForgetPassword",
+                async (ForgetPasswordRequestDto request, IUserService userService, IJwtService jwtService) =>
+                {
+
+                    var result = await userService.ForgetPassword(request);
+
+                    if (result.IsError)
+                        return BadRequest(result.FirstError.Description);
+
+                    return Ok(result.Value);
+                })
+                .AddEndpointFilter(new ValidationFilter<ForgetPasswordRequestDto>())
+                .WithTags(ApiInfo.Tag);
+        }
+    }
+}
