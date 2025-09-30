b6a14cada5c8ad6572b89c0ffe81e9ec2ec47377
abolfazl Shabani <57566177+abolfazlshs80@users.noreply.github.com>
2025-09-30 17:44:58 +0330

    ✨ feat(user): add verify code and expire date to user model
    - add 'VerifyCode' property to store user verification code
    - add 'VerifyCodeExpireDate' property to track code expiration

---
+++ Team-Library/BackEnd/TeamLibrary.API/Migrations/20250930141319_Add-VerifyCode-User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamLibrary.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVerifyCodeUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VerifyCode",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VerifyCodeExpireDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VerifyCode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "VerifyCodeExpireDate",
+                table: "Users");
+        }
+    }
+}
