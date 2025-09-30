c031018107019cb27fce5ec8e966608dbdd89c1f
abolfazl Shabani <57566177+abolfazlshs80@users.noreply.github.com>
2025-09-30 18:27:56 +0330

    ✨ feat(account): implement reset password endpoint
    - add endpoint to allow users to reset their password
    - validate request using ValidationFilter
    - return appropriate response based on result of password reset operation

---
+++ Team-Library/BackEnd/TeamLibrary.API/Features/Account/ResetPasswordUserEndPoint.cs
@@ -0,0 +1,30 @@
+﻿using DrMeet.Api.Shared.Services.JwtService;
+using TeamLibrary.API.Featrues.Account.DTOs.Request;
+using TeamLibrary.API.Shared.Contracts;
+using TeamLibrary.API.Shared.Service.Interface;
+using TeamLibrary.API.Shared.Tools.Helper;
+
+namespace TeamLibrary.API.Featrues.Account;
+
+public static class ResetPasswordUserEndPoint
+{
+    public class EndPoint : BaseEndpoint, IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost($"{ApiInfo.Prefix}/ResetPassword",
+                async (ResetPasswordRequestDto request, IUserService userService, IJwtService jwtService) =>
+                {
+
+                    var result = await userService.ResetPassword(request);
+
+                    if (result.IsError)
+                        return BadRequest(result.FirstError.Description);
+
+                    return Ok(result.Value);
+                })
+                .AddEndpointFilter(new ValidationFilter<ResetPasswordRequestDto>())
+                .WithTags(ApiInfo.Tag);
+        }
+    }
+}
