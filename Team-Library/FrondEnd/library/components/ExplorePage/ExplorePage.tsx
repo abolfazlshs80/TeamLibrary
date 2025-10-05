7d102bc409dcb580eb01e702aee9b7614161ca08
erfan2220 <e.hashemi2220@gmail.com>
2025-10-06 00:33:44 +0330

    modified

--- Team-Library/FrondEnd/library/components/ExplorePage/ExplorePage.tsx
+++ Team-Library/FrondEnd/library/components/ExplorePage/ExplorePage.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { SearchIcon } from "lucide-react";
+import { BookOpen, Globe, SearchIcon, Star } from "lucide-react";
 import { LibraryBig } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -29,10 +29,13 @@ const ExplorePage = () => {
   const router = useRouter();
   const [searchTerm, setSearchTerm] = useState("");
   const [category, setCategory] = useState("");
+  const [rank, setRank] = useState("");
+  const [language, setLanguage] = useState("");
+  const [pages, setPages] = useState("");
   const [books, setBooks] = useState<Books[]>([]);
   const [loading, setLoading] = useState(true);
   const [bookCategory, setBookCategory] = useState<Category[]>([]);
- 
+
   useEffect(() => {
     const savedCategory = localStorage.getItem("selectedCategory");
     if (savedCategory) {
@@ -66,9 +69,11 @@ const ExplorePage = () => {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/Category/GetAllCategory?PageNumber=1&PageSize=100`);
-         let categoryList: Category[] = [];
-         categoryList = res.data.data.list;
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/Category/GetAllCategory?PageNumber=1&PageSize=100`
+        );
+        let categoryList: Category[] = [];
+        categoryList = res.data.data.list;
         console.log("categoryList نهایی:", categoryList);
         setBookCategory(categoryList);
       } catch (error) {
@@ -81,7 +86,6 @@ const ExplorePage = () => {
 
     fetchCategories();
   }, []);
-
 
   const filteredBooks = books.filter((book) => {
     const matchesSearchTerm =
@@ -100,7 +104,7 @@ const ExplorePage = () => {
           جستجو کتاب
         </h4>
 
-        <div className="flex gap-2 w-full">
+        <div className="grid grid-cols-2 sm:grid-cols-5 gap-2 w-full">
           <div className="relative w-full">
             <label className="text-[#777574] text-sm pr-3 font-medium">
               جستجو کتاب
@@ -110,11 +114,117 @@ const ExplorePage = () => {
               placeholder="جستجو..."
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              className="w-full px-10 mt-1 pb-2 pt-1 rounded-full border-2 border-[#a8a7a6] ring-0 focus:overflow-hidden focus:border-[#B2685A] outline-0 bg-white "
+              className="w-full text-sm px-10 mt-1  h-10 rounded-full border-2 border-[#a8a7a6] ring-0 focus:overflow-hidden focus:border-[#B2685A] outline-0 bg-white "
             />
             <SearchIcon
-              className="absolute right-2 top-[35px]"
+              className="absolute right-4 top-[40px]"
               color="#a8a7a6"
+              size={18}
+            />
+          </div>
+          {/* ===================== */}
+          <div className="relative w-full flex flex-col mt-1">
+            <label className="text-[#777574] text-sm pr-3 font-medium">
+              رنک کتاب
+            </label>
+            <select
+              value={rank}
+              onChange={(e) => setRank(e.target.value)}
+              className="w-full text-sm px-8 pt-1 mt-1 h-10 text-[#838382] rounded-full border-2 border-[#a8a7a6] ring-0 focus:overflow-hidden focus:border-[#B2685A] outline-0 bg-white "
+            >
+              {["1", "2", "3", "4", "5"].map((cat, index) => (
+                <option
+                  key={index}
+                  value={cat}
+                  className="text-[#838382] hover:bg-gray-200"
+                >
+                  {cat}
+                </option>
+              ))}
+            </select>
+            <Star
+              className="absolute right-3 top-[35px]"
+              color="#a8a7a6"
+              size={18}
+            />
+          </div>
+          {/* ===================== */}
+          <div className="relative w-full flex flex-col mt-1">
+            <label className="text-[#777574] text-sm pr-3 font-medium">
+              تعداد صفحات کتاب
+            </label>
+            <select
+              value={pages}
+              onChange={(e) => setPages(e.target.value)}
+              className="w-full text-sm px-8 mt-1 h-10 text-[#838382] rounded-full border-2 border-[#a8a7a6] ring-0 focus:overflow-hidden focus:border-[#B2685A] outline-0 bg-white "
+            >
+              <option
+                value={"100"}
+                className="text-[#838382] hover:bg-gray-200"
+              >
+                کمتر از 100 صفحه
+              </option>
+              <option
+                value={"200"}
+                className="text-[#838382] hover:bg-gray-200"
+              >
+                بین 100 تا 200 صفحه
+              </option>
+              <option
+                value={"300"}
+                className="text-[#838382] hover:bg-gray-200"
+              >
+                بین 200 تا 300 صفحه
+              </option>
+              <option
+                value={"400"}
+                className="text-[#838382] hover:bg-gray-200"
+              >
+                بین 300 تا 400 صفحه
+              </option>
+              <option
+                value={"500"}
+                className="text-[#838382] hover:bg-gray-200"
+              >
+                بین 400 تا 500 صفحه
+              </option>
+              <option
+                value={"1000"}
+                className="text-[#838382] hover:bg-gray-200"
+              >
+                بیشتر از 500 صفحه
+              </option>
+            </select>
+            <BookOpen
+              className="absolute right-3 top-[35px]"
+              color="#a8a7a6"
+              size={18}
+            />
+          </div>
+          {/* ===================== */}
+          <div className="relative w-full flex flex-col mt-1">
+            <label className="text-[#777574] text-sm pr-3 font-medium">
+              زبان کتاب
+            </label>
+            <select
+              value={language}
+              onChange={(e) => setLanguage(e.target.value)}
+              className="w-full text-sm px-8 mt-1 h-10 text-[#838382] rounded-full border-2 border-[#a8a7a6] ring-0 focus:overflow-hidden focus:border-[#B2685A] outline-0 bg-white "
+            >
+              {["فارسی", "انگلیسی", "فرانسوی", "عربی"].map((lang, index) => (
+                <option
+                  key={index}
+                  value={lang}
+                  className="text-[#838382] hover:bg-gray-200"
+                >
+                  {lang}
+                </option>
+              ))}
+            </select>
+            <Globe
+              className="absolute right-3 top-[35px]"
+              color="#a8a7a6"
+              size={18}
             />
           </div>
           {/* ===================== */}
@@ -125,8 +235,11 @@ const ExplorePage = () => {
             <select
               value={category}
               onChange={(e) => setCategory(e.target.value)}
-              className="w-full px-10 mt-1 pb-2 pt-1 text-[#838382] rounded-full border-2 border-[#a8a7a6] ring-0 focus:overflow-hidden focus:border-[#B2685A] outline-0 bg-white "
+              className="w-full text-sm px-8 mt-1 h-10 text-[#838382] rounded-full border-2 border-[#a8a7a6] ring-0 focus:overflow-hidden focus:border-[#B2685A] outline-0 bg-white "
             >
+              <option value={""} className="text-[#838382] hover:bg-gray-200">
+                همه دسته‌بندی‌ها
+              </option>
               {bookCategory.map((cat) => (
                 <option
                   key={cat.id}
@@ -138,8 +251,9 @@ const ExplorePage = () => {
               ))}
             </select>
             <LibraryBig
-              className="absolute right-3 top-[32px]"
+              className="absolute right-3 top-[35px]"
               color="#a8a7a6"
+              size={18}
             />
           </div>
         </div>
@@ -164,9 +278,7 @@ const ExplorePage = () => {
                   className="border cursor-pointer p-2 rounded-lg flex flex-col items-center hover:shadow-lg hover:scale-[1.02]  transition-all duration-300"
                 >
                   <Image
-
                     src={book.imageUrl ?? defaultBook}
-
                     alt={book.title}
                     className=" h-48 w-40 object-cover mb-2 rounded"
                     width={200}
